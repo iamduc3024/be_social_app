cb9d35f6344e9497a34431af3d921aaa78de00e1
TDungx2k3 <dungpgpma2k3@gmail.com>
2025-09-26 22:26:09 +0700

    add swagger

--- src/main/java/com/example/jobapp/controllers/UserController.java
+++ src/main/java/com/example/jobapp/controllers/UserController.java
@@ -7,25 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import io.swagger.annotations.ApiOperation;
 
 import com.example.jobapp.entities.User;
 import com.example.jobapp.services.UserService;
 
-import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("${api.prefix}/user")
-@Api(tags = "user", description = "APIs for user module")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping("/create_user")
-    @ApiOperation(value = "Create new user")
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
